7545cce4ee589a49361427d2d16e4fb4a4d0e4df
Dercan <demand666@yandex.ru>
2016-11-23 14:47:59 +0300

    Option II done. Need to test.

--- Task 1/II.Groups_management.sh
+++ Task 1/II.Groups_management.sh
@@ -4,28 +4,39 @@
 . Menu.sh
 . Utility.sh
 
+function groups_management
+{
+	echo "Not doen"
+}
+
 function add_group
 {
 	echo "Введите название для новой группы."
 	read new_group_name
-	addgroup "new_group_name"
-	#if [ $? -eq 0 ]
-
-	#	echo "Успешно."
+	addgroup "$new_group_name"
+	if [ $? -eq 0 ]
+	then
+		echo "Успешно."
+	fi
 }
 
 function delete_group
 {
-  echo "Введите название группы, которую хотите удалить."
-  read group_to_delete
-  delgroup "group_to_delete"
-  #if $? == 0:
-  #  echo "Успешно."
+	echo "Введите название группы, которую хотите удалить."
+	read group_to_delete
+	delgroup "$group_to_delete"
+	if [ $? -eq 0 ]
+	then
+		echo "Успешно."
+	fi
 }
 
 function change_group
 {
-	menu --title="Изменение состава группы" "Добавить пользователя в группу" add_user_to_group "Удалить пользователя из группы" delete_user_from_group
+	title="Изменение состава группы."
+	option1="Добавить пользователя в группу."
+	option2="Удалить пользователя из группы."
+	menu --title=$title $option1 add_user_to_group $option2 delete_user_from_group
 }
 
 # Просит пользователя ввести пользователя.
@@ -36,24 +47,38 @@ function get_user
 	echo "Введите id или имя существующего пользователя."
 	read potential_user
 	cut -d: -f1,3 /etc/passwd | tr : " " | contains $potential_user
+	result=$?
+	if [ 
 	return $?
 }
 
 # Аналогично для групп.
 function get_group
 {
-	echo "Введите id или имя существующего пользователя."
-	read potential_user
-	cut -d: -f1,3 /etc/passwd | tr : " " | contains $potential_user
+	echo "Введите id или название существующей группы."
+	read potential_group
+	cut -d: -f1,3 /etc/group | tr : " " | contains $potential_group
 	return $?
 }
 
 function add_user_to_group
 {
-echo hi
+	echo "Введите группу и пользователя, которого хотите добавить в эту группу(через пробел)."
+	read group user
+	useradd -aG -- "$user" "$group"
+	if [ $? -eq 0 ]
+	then
+		echo "Успешно."
+	fi
 }
 
 function delete_user_from_group
 {
-echo hi
+	echo "Введите группу и пользователя, которого хотите удалить из этой группы."
+	read group user
+	userdel -- "$user" "$group"
+	if [ $? -eq 0 ]
+	then
+		echo "Успешно."
+	fi
 }
