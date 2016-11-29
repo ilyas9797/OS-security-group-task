9eacce137a35a72d43fad8149058a3592e555475
Гентюк В.А <33gva@mail.ru>
2016-11-29 20:21:33 +0300

    1. изменил массив на users, 2. добавил удаление файла lab_testing

--- Task 1/work_with_array.sh
+++ Task 1/work_with_array.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+
 cut -d: -f1,3 /etc/passwd|sed 's/:/ /g' > lab_testing
+
 c=0;
 while read line; do
 	let b=0;
@@ -8,13 +10,15 @@ while read line; do
 		a[$b]=$i;
 	done;
 	if [ "${a[2]}" -ge "1000" ]; then
-		let c+=1; user[$c]=${a[1]};
+		let c+=1; users[$c]=${a[1]};
 		echo "$c. ${a[1]}";
 	fi;
 done < lab_testing
 
 #если хотите проверить, правильно ли записан массив:
-#for i in ${user[@]}; do echo " $i";done;
+#for i in ${users[@]}; do echo " $i";done;
+
+rm -f lab_testing
 
 
-	
+
