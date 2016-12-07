26259f2cc0caa371bccc06b98f6b0ec8ea7e2223
root <root@localhost.localdomain>
2016-12-07 23:33:55 +0300

    изменены функции до вида, который хотел Андрей. проверка на ошибку при блокировании/разблокировании не сделал

--- Task 1/task_I_c.sh
+++ Task 1/task_I_c.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+#функция, которая записывает действительных полоьзователей в массив,
+#выводит на их экран, а также показывает, кто заюдокирован, а кто - нет. 
 #function acting_users
 #{
+        #считываем всех пользователей из /etc/passwd (1 и 3 столбцы), вместо двоеточия ставим пробелы, записываем в  lab_testing
 	cut -d: -f1,3 /etc/passwd|sed 's/:/ /g' > lab_testing
 	
+        #записываем в массив пользователей, чей ID>1000
+        echo "доступные пользователи:"
 	c=0;
 	while read line; do
 		let b=0;
@@ -20,17 +25,79 @@
 	#если хотите проверить, правильно ли записан массив:
 	#for i in ${users[@]}; do echo " $i";done;
 	
+        #удаляем промежуточный файл lab_testing
 	rm -f lab_testing
+
+        #записываем номера заблокированных пользователей в locked
+        #разблокированных в unlocked
+        n1=0;
+        n2=0;
+        c=0;
+        for i in ${users[@]}; do
+                flag=2;
+                cat /etc/shadow | grep "$i"|sed 's/:/ /g' > new;
+                read s < new;
+                rm -f new;
+                let k=0
+                while [ $k -lt 15 ]; do
+                           (( k++ ));
+                           if [ "${s:$k:1}" == "!" ]; then
+                               flag=1;
+                           fi;
+                done
+                if [ "$flag" -eq 1 ]; then
+                           # пользователь заблокирован
+                           let c+=1
+                           ((n1++))
+                           locked[$n1]=$c
+                fi
+                if [ "$flag" -eq 2 ]; then
+                           # пользователь разблокирован
+                           let c+=1
+                           ((n2++))
+                           unlocked[$n2]=$c
+                fi
+        done
+
+        #вывод заблокированных пользователей
+        echo "из них заблокированы:"
+        for i in ${locked[@]}; do
+                echo "$i. ${users[$i]}";
+        done
+
+        #вывод заблокированных пользователей
+        echo "разблокированные пользователи:"
+        for i in ${unlocked[@]}; do
+                echo "$i. ${users[$i]}";
+        done
+
+
+
+        
+          
+
+
 #}
 
-#function lock_user
+#функция блокировки/разблокировки пользователя
+#function lock_or_unlock_user
 #{
-        echo "Введите имя пользователя, которого хотите заблокировать."
+        echo "введите имя пользователя, которого хотите заблокировать/разблокировать, ИЛИ его номер:"
 	read user_name
         flag="0"
         #flag=0 - пользователя нет
         #flag=1 - пользователь есть и уже заблокирован
-        #flag=2 - пользователь есть и не заблокирован 
+        #flag=2 - пользователь есть и не заблокирован
+
+        #проверяем номер или нет, если номер, то меняем user_name на имя
+        while [ "$c" -ge "0" ]; do
+               if [ "$c" == "$user_name" ]; then
+                      user_name=${users[$c]};
+               fi;        
+               ((c--))
+        done
+
+        #проверка наличия в массиве users и также состояния раблок/заблок
         for i in ${users[@]}; do 
                if [ "$user_name" == "$i"  -a "$flag" -eq 0 ]; then
                       flag=2;
@@ -47,58 +114,22 @@
                       done;
                 fi;
         done
+
+        #если пользователь есть, то произойдет одно из трех действий
         if [ "$flag" -eq 0 ]; then           
               echo "нет такого пользователя";
         fi;
         if [ "$flag" -eq 1 ]; then           
-              echo "пользователь уже заблокирован";
+              echo "разблокируем пользователя";
+              usermod -U "$user_name";
+              echo "Успешно";
         fi;
         if [ "$flag" -eq 2 ]; then                         
               echo "блокируем пользователя";
-              #usermod -L "$user_name";
+              usermod -L "$user_name";
               echo "Успешно";
         fi;
 #}
-
-#function unlock_user
-#{
-        echo "Введите имя пользователя, которого хотите разблокировать."
-	read user_name
-        flag="0"
-        #flag=0 - пользователя нет
-        #flag=1 - пользователь есть и заблокирован
-        #flag=2 - пользователь есть и не заблокирован 
-        for i in ${users[@]}; do 
-               if [ "$user_name" == "$i"  -a "$flag" -eq 0 ]; then
-                      flag=2;
-                      cat /etc/shadow | grep "$user_name"|sed 's/:/ /g' > new;
-                      read s < new;
-                      rm -f new;
-                      let k=0
-                      while [ $k -lt 15 ]; do 
-                           (( k++ ));
-                           
-                           if [ "${s:$k:1}" == "!" ]; then
-                               flag=1;
-                           fi;                                
-                      done;
-                fi;
-        done
-        if [ "$flag" -eq 0 ]; then           
-              echo "нет такого пользователя";
-        fi;
-        if [ "$flag" -eq 2 ]; then           
-              echo "пользователь незаблокирован";
-        fi;
-        if [ "$flag" -eq 1 ]; then                         
-              echo "разблокируем пользователя";
-              #usermod -U "$user_name";
-              echo "Успешно";
-        fi;
-#}
-
-                      
-                      
 
 
 
